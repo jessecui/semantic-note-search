600c2f5a92bf3219a0ad13368e22353dc8139b6b
Jesse Cui <jcui.projects@gmail.com>
2024-03-19 21:04:47 -0400

    Consolidate components and auth

--- src/app/verify-login/route.tsx
+++ src/app/verify-login/route.tsx
@@ -1,9 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const password = request.nextUrl.searchParams.get("password");
+  const verified =
+    request.nextUrl.searchParams.get("password") == process.env.PASSWORD;
+
+  let supabaseCredentials = {};
+  if (verified) {
+    supabaseCredentials = {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseKey: process.env.SUPABASE_KEY,
+    };
+  }
 
   return NextResponse.json({
-    verified: password == process.env.PASSWORD,
+    verified,
+    supabaseCredentials,
   });
 }
